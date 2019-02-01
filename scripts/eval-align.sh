db63a286f7687a7ec52068e020eb891d80f5b73d
Mitra TJ <33361116+MitraTj@users.noreply.github.com>
2019-02-01 11:17:35 +0800

    Update eval-align.sh

--- scripts/eval-align.sh
+++ scripts/eval-align.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=1
 python -m pdb models/eval-rel.py -m sgcls -model align -b 1 -clip 5 -use_bias \
       -p 100 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -ckpt checkpoints/motifnet-conf-sgcls/vgrel-11.tar \
-      -nepoch 50 -tensorboard_interval 10 -test -cache baseline_sgcls \
+      -nepoch 50 -tensorboard_interval 10 -test -cache motifnet-conf-sgcls 
       
       
 # python -m pdb models/eval-rel.py -m sgcls -model align -b 1 -clip 5 -use_bias \
