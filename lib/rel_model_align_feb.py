b63b8b42040f8e14d2bd20e8ac29fef78bd9e4e5
Mitra TJ <33361116+MitraTj@users.noreply.github.com>
2019-02-28 14:13:48 +0800

    Update rel_model_align_feb.py

--- lib/rel_model_align_feb.py
+++ lib/rel_model_align_feb.py
@@ -167,31 +167,20 @@ class DynamicFilterContext(nn.Module):
             O_fmaps_trans = O_fmaps.view(num_rels, self.reduce_dim, self.pooling_size*self.pooling_size).transpose(2, 1)
 
             pooling_size_sq = self.pooling_size*self.pooling_size
-            similar_fu = []
+            SO_fmaps_logits = []
 #            import pdb; pdb.set_trace()
             for i in range(0, pooling_size_sq):
                 for j in range(0, pooling_size_sq):
-                    #SO_fmaps_extend.append(torch.cat((S_fmaps_trans[:, i].transpose(0, 1), O_fmaps_trans[:, j].transpose(0, 1)), dim=1))
-                    self.similar_fun.append(torch.cat((S_fmaps_trans[:, i, :], O_fmaps_trans[:, j, :]), dim=1))
-#                    SO_fmaps_extend = torch.stack(SO_fmaps_extendd,1)
-            #SO_fmaps_extendd = SO_fmaps_extendd.view(num_rels, self.reduce_dim, self.reduce_dim*2)
-            #SO_fmaps_extend = torch.cat(SO_fmaps_extendd, 1)
-            import pdb; pdb.set_trace()
-            #SO_fmaps_logits = self.similar_fun(SO_fmaps_extend)
-            import pdb; pdb.set_trace()
-            # SO_fmaps_logits= SO_fmaps_logits.view(num_rels, pooling_size_sq, pooling_size_sq) # wrong size
-            SO_fmaps_logits= SO_fmaps_logits.view(num_rels, 2*self.reduce_dim, self.reduce_dim)
-            import pdb; pdb.set_trace() 
-            SO_fmaps_scores = F.softmax(SO_fmaps_logits, dim=1)
-            import pdb; pdb.set_trace()
-            weighted_S_fmaps = torch.matmul(SO_fmaps_scores.transpose(2, 1), S_fmaps_trans) # (num_rels, 49, 49) x (num_rels, 49, self.reduce_dim)
+                    SO_fmaps_logits.append(self.similar_fun(torch.cat((S_fmaps_trans[:,i,None], O_fmaps_trans[:,j,None]), dim=2)))
+                    SO_fmaps_logits1 = torch.stack(SO_fmaps_logits,1)       
 
+            SO_fmaps_logits1 = SO_fmaps_logits1.view(num_rels, pooling_size_sq, pooling_size_sq) # wrong size 
+            SO_fmaps_scores = F.softmax(SO_fmaps_logits1, dim=1)
+            weighted_S_fmaps = torch.matmul(SO_fmaps_scores.transpose(2, 1), S_fmaps_trans) # (num_rels, 49, 49) x (num_rels, 49, self.redu$
             last_SO_fmaps = torch.cat((weighted_S_fmaps, O_fmaps_trans), dim=2)
-            last_SO_fmaps = last_SO_fmaps.transpose(2, 1).contiguous().view(num_rels, self.reduce_dim*2, self.pooling_size, self.pooling_size)
-           # S_fmaps_extend = S_fmaps_trans.repeat(1, 1, pooling_size_sq).view(num_rels, pooling_size_sq*pooling_size_sq, self.reduce_dim)
-          #  O_fmaps_extend = O_fmaps_trans.repeat(1, pooling_size_sq, 1)
-            
-           
+            last_SO_fmaps = last_SO_fmaps.transpose(2, 1).contiguous().view(num_rels, self.reduce_dim*2, self.pooling_size, self.pooling_si$
+#            import pdb; pdb.set_trace()
+                
         else:
             raise ValueError
 
