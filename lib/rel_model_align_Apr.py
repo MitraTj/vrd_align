c032ecbc9fd5644b42ea1933cc7259db3406785a
Mitra TJ <33361116+MitraTj@users.noreply.github.com>
2019-05-16 12:41:03 +0800

    Update rel_model_align_Apr.py

--- lib/rel_model_align_Apr.py
+++ lib/rel_model_align_Apr.py
@@ -178,7 +178,7 @@ class DynamicFilterContext(nn.Module):
         
         num_objs = obj_logits.shape[0]
         num_rels = rel_inds.shape[0]
-        temperature = 1
+        temperature = 1    ##0.7
         rois = torch.cat((im_inds[:, None].float(), boxes_priors), 1)
         obj_fmaps = self.obj_feature_map(fmaps, rois)
         reduce_obj_fmaps = self.reduce_obj_fmaps(obj_fmaps)
@@ -203,10 +203,10 @@ class DynamicFilterContext(nn.Module):
             SO_fmaps_logits = SO_fmaps_logits.view(num_rels, pooling_size_sq, pooling_size_sq) # (first dim is S_fmaps, second dim is O_fmaps)
 #            print('SO_fmaps_logits.shape:', SO_fmaps_logits.shape)
 ##            import pdb; pdb.set_trace()
-            y1 = self.gumbel_softmax(SO_fmaps_logits, temperature, training=False)
+            SO_fmaps_scores = self.gumbel_softmax(SO_fmaps_logits, temperature, training=False)
             #SO_fmaps_scores = F.softmax(y, dim=1)
            
-            weighted_S_fmaps = torch.matmul(y1.transpose(2, 1), S_fmaps_trans) # (num_rels, 49, 49) x (num_rels, 49, self.reduce_dim)
+            weighted_S_fmaps = torch.matmul(SO_fmaps_scores.transpose(2, 1), S_fmaps_trans) # (num_rels, 49, 49) x (num_rels, 49, self.reduce_dim)
             
             last_SO_fmaps = torch.cat((weighted_S_fmaps, O_fmaps_trans), dim=2)
             last_SO_fmaps = last_SO_fmaps.transpose(2, 1).contiguous().view(num_rels, self.reduce_dim*2, self.pooling_size, self.pooling_size)
