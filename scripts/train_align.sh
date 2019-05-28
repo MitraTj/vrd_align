6090e7708e51a86971a3bc12eb7a01225e9f343b
Mitra TJ <33361116+MitraTj@users.noreply.github.com>
2019-05-28 18:57:15 +0800

    Update train_align.sh

--- scripts/train_align.sh
+++ scripts/train_align.sh
@@ -9,3 +9,7 @@ python -m pdb models/train_align.py -m sgcls -model align -b 1 -clip 5 -hidden_d
 
 python -m pdb models/train_align.py -m sgcls -model align -b 1 -clip 5 -hidden_dim 512 -use_bias       -p 100 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -ckpt checkpoints/vgdet1/vg-41.tar  -save_dir checkpoints/debug/sgcls/debugW41hardnew -nepoch 100 -tensorboard_interval 10 -sl_train -debug_type test1_1 -pooling_size 5
 
+
+
+
+python -m pdb models/train_align.py -m sgcls -model align -b 1 -clip 5 -hidden_d checkpoints/vg-faster-rcnn.tar  -save_dir checkpoints/debug/sgcls/debugW2t -nepoch 100 -tensorboard_interval 10 -sl_train -debug_type test1_1 -pooling_size 5
