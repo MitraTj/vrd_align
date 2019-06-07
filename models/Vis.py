15ead3d84bfa9c89552810538a2eba77f3781ef3
Mitra TJ <33361116+MitraTj@users.noreply.github.com>
2019-06-07 14:34:11 +0800

    Update Vis.py

--- models/Vis.py
+++ models/Vis.py
@@ -1,7 +1,22 @@
+from dataloaders.visual_genome import VGDataLoader, VG
+from lib.rel_model import RelModel
+mport torch
+
+from config import ModelConfig
+from lib.pytorch_misc import optimistic_restore
+from lib.evaluation.sg_eval import BasicSceneGraphEvaluator
+from tqdm import tqdm
+from config import BOX_SCALE, IM_SCALE
+from lib.fpn.box_utils import bbox_overlaps
+from collections import defaultdict
+
 import numpy as np
-#from PIL import Image
+from PIL import Image
 from IPython.display import Image, display
 import json
 from matplotlib.pyplot import imshow
-import time
 import json
+
+path1 = '/data/VG_100k'
+path2 = '/data/stanford_filtered/VG-SGG-dicts.json'
+
