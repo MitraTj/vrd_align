94a088a153d6470976d7d9da8dc1e867beaac9a0
Mitra TJ <33361116+MitraTj@users.noreply.github.com>
2019-06-09 16:57:31 +0800

    Update eval-align.sh

--- scripts/eval-align.sh
+++ scripts/eval-align.sh
@@ -7,4 +7,9 @@ python -m pdb models/eval-rel.py -m sgcls -model align -b 1 -clip 5 -use_bias \
 # python -m pdb models/eval-rel.py -m sgcls -model align -b 1 -clip 5 -use_bias \
      # -p 100 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -ckpt checkpoints/debug/sgcls/debug2/sgcls-16 \
      # -nepoch 50 -tensorboard_interval 10 -test -cache baseline_predcls \
+     
+ python -m pdb models/train_align.py -m sgcls -model align -b 1 -clip 5 -hidden_dim 512 -use_bias \ 
+ -p 100 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -ckpt checkpoints/debug/sgcls/debugHKSelumean/sgcls-9.tar \
+ -save_dir checkpoints/debug/sgcls/debugW41hardnew -nepoch 100 -tensorboard_interval 10 -sl_rl_test \ 
+ -debug_type test1_1 -pooling_size 5 -test -cache caches/Align_sgcls.pkl
           
